fac56a1cbce62e7ea2dc3d978aac1d4348602250
David Childerhose <davidoforange@outlook.com>
2022-01-31 22:34:56 +0900

    created single and page files

--- app/public/wp-content/themes/orange-university-theme/page.php
+++ app/public/wp-content/themes/orange-university-theme/page.php
@@ -1,10 +1,9 @@
 <?php 
-
   
   while(have_posts()) {
       the_post(); ?>
-      <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+      <h1>This is a page, not a post</h1>
+      <h1><?php the_title(); ?></h1>
       <?php the_content(); ?>
-      <hr>
   <?php }
 ?>
