5561822215b34f0c683ae44cab7afc5a2fcf5186
David Childerhose <davidoforange@outlook.com>
2022-02-02 21:07:57 +0900

    Created Events and Programs pages

--- app/public/wp-content/themes/orange-university-theme/header.php
+++ app/public/wp-content/themes/orange-university-theme/header.php
@@ -15,8 +15,8 @@
           <nav class="main-navigation">
             <ul>
               <li><a href="<?php echo site_url("/about-us"); ?>">About Us</a></li>
-              <li><a href="#">Programs</a></li>
-              <li><a href="#">Events</a></li>
+              <li><a href="<?php echo site_url("/our-programs"); ?>">Programs</a></li>
+              <li><a href="<?php echo site_url("/events"); ?>">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
             </ul>
